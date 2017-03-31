3da7898b51f88837ad74a963e1f162139478bdc6
weizongwei5 <weizongwei5@gmail.com>
2017-03-31 18:07:23 +0800

    导入文件

---
+++ app/src/main/java/com/androidyuan/guetzliandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidyuan.guetzliandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ libguetzli/src/main/jni/Application.mk
@@ -0,0 +1,6 @@
+APP_ABI :=  armeabi
+#  armeabi-v7a
+APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS += -frtti
+APP_STL := gnustl_static
+APP_STL := c++_static
